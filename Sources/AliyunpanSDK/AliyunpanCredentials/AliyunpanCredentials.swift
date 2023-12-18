01cae4b56d0de338d37437f92ba426059babee55
zhaixian <zixuan.wzx@alibaba-inc.com>
2023-12-18 11:19:52 +0800

    update: update AliyunpanCredentials description

--- Sources/AliyunpanSDK/AliyunpanCredentials/AliyunpanCredentials.swift
+++ Sources/AliyunpanSDK/AliyunpanCredentials/AliyunpanCredentials.swift
@@ -12,15 +12,12 @@ protocol AliyunpanCredentialsProtocol {
 }
 
 public enum AliyunpanCredentials {
-    /// PKCE 授权，
-    /// iOS
+    /// PKCE 授权，无需服务端，需要安装阿里云盘客户端
     /// https://auth0.com/docs/get-started/authentication-and-authorization-flow/authorization-code-flow-with-proof-key-for-code-exchange-pkce
     case pkce
-    /// 标准授权，有 Server 的业务才可使用
-    /// iOS
+    /// 标准授权，需要服务端，需要安装阿里云盘客户端
     case server(AliyunpanBizServer)
-    /// 二维码授权
-    /// iOS、MacOS、tvOS
+    /// 二维码授权，无需服务端，无需安装阿里云盘客户端
     case qrCode(AliyunpanQRCodeContainer)
     
     var implement: AliyunpanCredentialsProtocol {
