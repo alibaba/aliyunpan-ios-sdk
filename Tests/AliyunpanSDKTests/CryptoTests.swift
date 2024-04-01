a204c8268f5aaaadd796166ac8f2b747e2af38a6
zhaixian <zixuan.wzx@alibaba-inc.com>
2024-04-01 17:46:25 +0800

    update: update unit test

--- Tests/AliyunpanSDKTests/CryptoTests.swift
+++ Tests/AliyunpanSDKTests/CryptoTests.swift
@@ -20,27 +20,27 @@ class CryptoTests: XCTestCase {
     }
     
     func testProofCode() throws {
-        let url = Bundle(for: self.classForCoder).url(forResource: "README", withExtension: "md")!
+        let url = Bundle(for: self.classForCoder).url(forResource: "TestFile1", withExtension: "txt")!
 
         XCTAssertEqual(
             AliyunpanCrypto.getProofCode(
                 accessToken: "Bearer eyJraWQiOiJLcU8iLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwZWJkMjUyY2EyNTg0MmY0OTBmYmJhYWJlZTkwOGE5YyIsImF1ZCI6IjQ2NWJjNzMzZmQ2MTQxNDJiZmQ1Y2MxNGYzZjk1NWFlIiwicyI6ImNkYSIsImQiOiIxMDE4NDAsMTAyOTM0MjAiLCJpc3MiOiJhbGlwYW4iLCJleHAiOjE3MTQwMzg0NDYsImwiOjIsImlhdCI6MTcxMTQ0NjQ0MywianRpIjoiODM2ZmVkZDExMjhkNDU2MTg0NjA4OTgwMzBlZGM5NTkifQ.yZJFeeUriWOcPhg-CHHO1XyotwuFR0v",
                 fileURL: url),
-            "LS06IHwgOi0="
+            "bG8gV29ybGQ="
         )
     }
     
     func testSHA1() throws {
-        let url = Bundle(for: self.classForCoder).url(forResource: "README", withExtension: "md")!
+        let url = Bundle(for: self.classForCoder).url(forResource: "TestFile1", withExtension: "txt")!
 
         XCTAssertEqual(
             AliyunpanCrypto.sha1AndHex(url),
-            "EBB77C17D1A3FCC87239D4BEB463A95FF80D2FB9"
+            "2EF7BDE608CE5404E97D5F042F95F89F1C232871"
         )
         
         XCTAssertEqual(
             AliyunpanCrypto.sha1AndHex(try Data(contentsOf: url)),
-            "EBB77C17D1A3FCC87239D4BEB463A95FF80D2FB9"
+            "2EF7BDE608CE5404E97D5F042F95F89F1C232871"
         )
     }
 }
